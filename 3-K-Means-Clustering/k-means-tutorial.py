8b309922c0e01789d845f420da62944bb6348fc6
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2018-11-25 19:15:38 +0300

    Update k-means-tutorial.py

--- 3-K-Means-Clustering/k-means-tutorial.py
+++ 3-K-Means-Clustering/k-means-tutorial.py
@@ -1,65 +1,73 @@
 # -*- coding: utf-8 -*-
- 
+"""
+Created on Sun Nov 25 17:12:03 2018
+
+@author: EbubekirDogan
+"""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+# %%
 
-# %% create dataset
-
-# class1
+#Class 1 
 x1 = np.random.normal(25,5,1000)
 y1 = np.random.normal(25,5,1000)
 
-# class2
+#Class 2
 x2 = np.random.normal(55,5,1000)
 y2 = np.random.normal(60,5,1000)
 
-# class3
+#Class 3
 x3 = np.random.normal(55,5,1000)
 y3 = np.random.normal(15,5,1000)
+ 
 
 x = np.concatenate((x1,x2,x3),axis = 0)
-y = np.concatenate((y1,y2,y3),axis = 0)
-
+y = np.concatenate((y1,y2,y3),axis = 0) 
+ 
 dictionary = {"x":x,"y":y}
 
 data = pd.DataFrame(dictionary)
 
-plt.scatter(x1,y1)
-plt.scatter(x2,y2)
+plt.scatter(x1,y1) 
+plt.scatter(x2,y2) 
 plt.scatter(x3,y3)
+
 plt.show()
 
-## %% kmeans algoritması bunu gorecek
-#plt.scatter(x1,y1,color = "black")
-#plt.scatter(x2,y2,color = "black")
-#plt.scatter(x3,y3,color = "black")
+# %% k-means algoritması bunu görecek
+#plt.scatter(x1,y1,color ="black") 
+#plt.scatter(x2,y2,color ="black")
+#plt.scatter(x3,y3,color ="black")
 #plt.show()
-
-# %% KMEANS
+# %% KMEANS 
 
 from sklearn.cluster import KMeans
-wcss = []
+wcss= [] 
 
-for k in range(1,15):
+for k  in range(1,15):
     kmeans = KMeans(n_clusters=k)
-    kmeans.fit(data)
-    wcss.append(kmeans.inertia_)
-    
+    kmeans.fit(data) # modelimi olustur
+    wcss.append(kmeans.inertia_) # bize her bir k degerini bul demek inertia_
+
 plt.plot(range(1,15),wcss)
-plt.xlabel("number of k (cluster) value")
-plt.ylabel("wcss")
+plt.xlabel("number of k (cluster) value ") # x ekseninde yazacak string
+plt.ylabel("wcss") # y ekseninde yazacak olan string
 plt.show()
+# %% k = 3 icin modelimiz.
 
-#%% k = 3 icin modelim
-
-kmeans2 = KMeans(n_clusters=3)
-clusters = kmeans2.fit_predict(data)
+kmean2 = KMeans(n_clusters=3)
+clusters=kmean2.fit_predict(data) # 0-1-2 diye datamızı classlarına ayırdı.
 
 data["label"] = clusters
 
-plt.scatter(data.x[data.label == 0 ],data.y[data.label == 0],color = "red")
-plt.scatter(data.x[data.label == 1 ],data.y[data.label == 1],color = "green")
-plt.scatter(data.x[data.label == 2 ],data.y[data.label == 2],color = "blue")
-plt.scatter(kmeans2.cluster_centers_[:,0],kmeans2.cluster_centers_[:,1],color = "yellow")
+
+plt.scatter(data.x[data.label == 0],data.y[data.label == 0],color="red")
+plt.scatter(data.x[data.label == 1],data.y[data.label == 1],color="blue")
+plt.scatter(data.x[data.label == 2],data.y[data.label == 2],color="purple")
+plt.scatter(kmean2.cluster_centers_[:,0],kmean2.cluster_centers_[:,1],color="yellow")
 plt.show()
+
+
+
